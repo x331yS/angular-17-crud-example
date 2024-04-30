9c1f18dbce4343c57fc624f40c2dea3437a05676
Florian LEJOSNE <florian.lejosne@byg4lab.com>
2024-04-30 10:38:16 +0200

    Refactor tutorial-details.component.spec.ts imports and dependencies

--- src/app/components/tutorial-details/tutorial-details.component.spec.ts
+++ src/app/components/tutorial-details/tutorial-details.component.spec.ts
@@ -1,9 +1,9 @@
-import { ActivatedRoute } from '@angular/router';
-import { of } from 'rxjs';
-import { TutorialDetailsComponent } from './tutorial-details.component';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { TutorialDetailsComponent } from './tutorial-details.component';
 import { HttpClientModule } from '@angular/common/http';
 import { NO_ERRORS_SCHEMA } from '@angular/core';
+import { RouterTestingModule } from "@angular/router/testing";
 
 describe('TutorialDetailsComponent', () => {
   let component: TutorialDetailsComponent;
@@ -11,24 +11,9 @@ describe('TutorialDetailsComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [HttpClientModule],
+      imports: [HttpClientModule,RouterTestingModule],
       declarations: [TutorialDetailsComponent],
-      schemas: [NO_ERRORS_SCHEMA],
-      providers: [
-        { 
-          provide: ActivatedRoute, 
-          useValue: { 
-            paramMap: of({ get: (str: string) => 'mock-id' }),
-            snapshot: { 
-              paramMap: {
-                get: (str: string) => {
-                  return 'mock-id';
-                }
-              }
-            }
-          } 
-        }
-      ]
+      schemas: [NO_ERRORS_SCHEMA]
     })
     .compileComponents();
     
